7cc9bf5e5150c17447767b2b4df3c954fe05a92c
GuanDong <gchuyun@qq.com>
2016-03-26 23:08:31 +0800

    weixin

--- app/utils/WeiXinUtils.java
+++ app/utils/WeiXinUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import org.apache.commons.lang.StringUtils;
+import play.Logger;
 import play.Play;
 import play.libs.Codec;
 
@@ -14,6 +15,7 @@ public class WeiXinUtils {
     public static boolean checkSignature(String signature, String
             timestamp, String nonce) {
         String token = Play.configuration.getProperty("weixin.token");
+
         String[] arr = new String[]{token, timestamp, nonce};
         // 将token、timestamp、nonce三个参数进行字典序排序
         Arrays.sort(arr);
@@ -24,6 +26,7 @@ public class WeiXinUtils {
         // 将三个参数字符串拼接成一个字符串进行sha1加密
         String tmpStr = Codec.hexSHA1(content.toString());
 
+        Logger.debug("token: %s, sign: %s, tmp: %s", token, signature, tmpStr);
         // 将sha1加密后的字符串可与signature对比，标识该请求来源于微信
         return tmpStr != null && StringUtils.equalsIgnoreCase(tmpStr, signature);
     }
