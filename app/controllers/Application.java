22b2cf9bc5f2c890e1b7c6b49818c5ef3c73267d
GuanDong <gchuyun@qq.com>
2016-03-26 22:18:22 +0800

    weixin

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,40 @@
 package controllers;
 
+import org.apache.commons.lang.StringUtils;
 import play.mvc.*;
+import play.mvc.results.Error;
+import utils.WeiXinUtils;
+
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.ResourceBundle;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
         ProductsC.list(null);
+    }
+
+    public static void token(){
+        // 微信加密签名
+        String signature = params.get("signature");
+        // 随机字符串
+        String echostr = params.get("echostr");
+        // 时间戳
+        String timestamp = params.get("timestamp");
+        // 随机数
+        String nonce = params.get("nonce");
+
+        // 通过检验signature对请求进行校验，若校验成功则原样
+        // 返回echostr，表示接入成功，否则接入失败
+        if (StringUtils.isBlank(signature)) {
+            error("wechat param not found");
+        } else if (WeiXinUtils.checkSignature(signature,timestamp,
+                nonce)) {
+            renderText(echostr);
+        }
+
     }
 
     public static void login() {
---
+++ app/utils/WeiXinUtils.java
@@ -0,0 +1,29 @@
+package utils;
+
+import play.Play;
+import play.libs.Codec;
+
+import java.util.Arrays;
+
+/**
+ * Created by gchuyun on 16/3/26.
+ */
+public class WeiXinUtils {
+
+    public static boolean checkSignature(String signature, String
+            timestamp, String nonce) {
+        String token = Play.configuration.getProperty("weixin.token");
+        String[] arr = new String[]{token, timestamp, nonce};
+        // 将token、timestamp、nonce三个参数进行字典序排序
+        Arrays.sort(arr);
+        StringBuilder content = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            content.append(arr[i]);
+        }
+        // 将三个参数字符串拼接成一个字符串进行sha1加密
+        String tmpStr = Codec.hexSHA1(content.toString());
+
+        // 将sha1加密后的字符串可与signature对比，标识该请求来源于微信
+        return tmpStr != null && tmpStr.equals(signature.toUpperCase());
+    }
+}
