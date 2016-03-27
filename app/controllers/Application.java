8edce4511b49490917d02c1c924f1a6a7734cb38
GuanDong <gchuyun@qq.com>
2016-03-27 14:55:21 +0800

    weixin

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import me.chanjar.weixin.common.exception.WxErrorException;
 import models.AccountM;
 import org.apache.commons.lang.StringUtils;
 import play.Play;
@@ -10,24 +11,31 @@ public class Application extends Controller {
 
     public static String BASE_URL = Play.configuration.getProperty("application.baseUrl");
 
-    @Before(unless = "subcribe")
+    @Before(priority = 1)
+    public static void processOAuth() throws WxErrorException {
+        String code = params.get("code");
+        String state = params.get("state");
+
+        if (StringUtils.equals(state, "auth") && !StringUtils.isBlank(code)){
+            String openId = WechatC.getOpenIdByCode(code);
+            AccountM account = AccountM.find("byOpenId", openId).first();
+            if (account != null){
+                Cache.set(session.getAuthenticityToken(), new AuthenticityInfo(account.id, account.openId, account.name));
+                return;
+            } else {
+                subcribe();
+            }
+        }
+
+    }
+
+    @Before(unless = "subcribe", priority = 2)
     public static void checkLogin(){
 
         String authToken = session.getAuthenticityToken();
         AuthenticityInfo authInfo = Cache.get(authToken, AuthenticityInfo.class);
         if (authInfo != null){
             return;
-        }
-
-        String openId = params.get("openId");
-        if (!StringUtils.isBlank(openId)){
-            AccountM account = AccountM.find("byOpenId", openId).first();
-            if (account != null){
-                Cache.set(authToken, new AuthenticityInfo(account.id, account.openId, account.name));
-                return;
-            } else {
-                subcribe();
-            }
         }
 
         redirect(WechatC.callBackOpenIdUrl(Application.BASE_URL + request.url, null));
--- app/controllers/WechatC.java
+++ app/controllers/WechatC.java
@@ -7,6 +7,7 @@ import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.*;
 import me.chanjar.weixin.mp.bean.*;
+import me.chanjar.weixin.mp.bean.result.WxMpOAuth2AccessToken;
 import me.chanjar.weixin.mp.bean.result.WxMpUser;
 import models.AccountM;
 import models.OrderM;
@@ -185,6 +186,13 @@ public class WechatC extends Controller {
     @Util
     public static String callBackOpenIdUrl(String url, String status){
         return wxMpService.oauth2buildAuthorizationUrl(url, WxConsts.OAUTH2_SCOPE_BASE, status);
+    }
+
+    @Util
+    public static String getOpenIdByCode(String code) throws WxErrorException {
+        WxMpOAuth2AccessToken wxMpOAuth2AccessToken = wxMpService.oauth2getAccessToken(code);
+        WxMpUser wxMpUser = wxMpService.oauth2getUserInfo(wxMpOAuth2AccessToken, null);
+        return wxMpUser.getOpenId();
     }
 
     @Util
