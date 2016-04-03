d781b735611245137870e008d1b4241b53aff3e9
GuanDong <gchuyun@qq.com>
2016-04-03 17:16:51 +0800

    weixin

--- app/controllers/Coupons.java
+++ app/controllers/Coupons.java
@@ -2,8 +2,13 @@ package controllers;
 
 import models.AccountCouponM;
 import models.CouponM;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.time.DateFormatUtils;
+import play.Logger;
+import soap.HUHU_spcCreate_spcAccount_spcCoupon_spcWeb_spcServiceStub;
+import soap.SoapInvoker;
 
+import java.rmi.RemoteException;
 import java.util.Date;
 import java.util.List;
 
@@ -15,16 +20,21 @@ public class Coupons extends Base {
         render(coupons, myCoupons);
     }
 
-    public static void take(String couponId) {
+    public static void take(String couponId) throws RemoteException {
         AccountCouponM coupon = AccountCouponM.find("accountId = ? and couponId = ?", getAccountOpenId(), couponId).first();
         if (coupon != null) {
             renderJSON("");
         }
-        AccountCouponM accountCouponM = new AccountCouponM();
-        accountCouponM.accountId = getAccountOpenId();
-        accountCouponM.couponId = couponId;
-        accountCouponM.save();
-        renderJSON(accountCouponM.id);
+
+        HUHU_spcCreate_spcAccount_spcCoupon_spcWeb_spcServiceStub.CreatedAccountCoupon_Input input = new HUHU_spcCreate_spcAccount_spcCoupon_spcWeb_spcServiceStub.CreatedAccountCoupon_Input();
+        input.setAccntid(getAccountOpenId());
+        input.setCouponid(couponId);
+        HUHU_spcCreate_spcAccount_spcCoupon_spcWeb_spcServiceStub.CreatedAccountCoupon_Output output = SoapInvoker.takeCoupon(input);
+        if (StringUtils.equals("S", output.getProcStatus())) {
+            renderText(couponId);
+        } else {
+            renderText("ERROR");
+        }
     }
 
 }
