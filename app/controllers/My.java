63ef77a96178a53181fdad9469f961100bb4c013
GuanDong <gchuyun@qq.com>
2016-04-03 09:20:41 +0800

    weixin

--- app/controllers/My.java
+++ app/controllers/My.java
@@ -1,16 +1,31 @@
 package controllers;
 
+import com.google.gson.Gson;
 import models.AccountCouponM;
+import models.OrderItemM;
 import models.OrderM;
+import play.Logger;
+import play.db.jpa.GenericModel;
+import play.db.jpa.JPA;
 
+import javax.persistence.Query;
 import java.util.List;
 
 //@With(Application.class)
 public class My extends Base {
 
     public static void index() {
-        List<OrderM> orderList = OrderM.find("accountId = ? order by createdDate", getAccountOpenId()).fetch();
+//        List<OrderM> orderList = OrderM.find("accountId = ? order by createdDate", getAccountOpenId()).fetch();
+//        List<OrderItemM> orderItemList = OrderItemM.find("accountId = ? order by orderId").fetch();
         List<AccountCouponM> couponList = AccountCouponM.find("accountId = ? and useFlag = 'N' order by endtDate", getAccountOpenId()).fetch();
+        Query query = JPA.em().createQuery("select o.* as order, " +
+                "(select i.* from OrderItemM i where i.orderId=o.orderId and i.productType='产品') as product,  " +
+                "(select i.* from OrderItemM i where i.orderId=o.orderId and i.productType='促销') as coupon" +
+                "from OrderM where accountId = ?accountId order by createdDate");
+
+        query.setParameter("accountId", getAccountOpenId());
+        List orderList = query.getResultList();
+        Logger.info("orderImtes: %s", new Gson().toJson(orderList));
         render(orderList, couponList);
     }
 
